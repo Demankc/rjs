1b0fab9a3efbd9a11a9454fce1f1f825142ffe48
Manoj Chowdhury <manojc009@gmail.com>
2019-11-02 18:49:11 +0100

    Adding CSS modules

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import classes from './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
@@ -38,16 +38,9 @@ class App extends Component {
   }
 
   render() {
-    const style = {
-      backgroundColor: 'green',
-      color: 'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer'
-    };
-
     let persons = null;
+    let btnClass = '';
+
     if( this.state.showPersons ){
       persons = (
         <div>
@@ -61,14 +54,24 @@ class App extends Component {
           })}
         </div>
       );
-      style.backgroundColor = 'red';
+
+      btnClass = classes.Red;
+    }
+
+    const assignedClasses = [];
+    if( this.state.persons.length <= 2){
+      assignedClasses.push(classes.red);
+    }
+    if( this.state.persons.length <= 1){
+      assignedClasses.push(classes.bold);
     }
 
     return (
-      <div className="App">
+      <div className={classes.App}>
         <h1>Hi I'm a React App</h1>
+        <p className={assignedClasses.join('')}>It works</p>
         <button 
-          style={style} 
+          className={btnClass}
           onClick={this.togglePersonsHandler}>Toggle Persons</button>
         {persons}
       </div>

68060be7e5066593cab6467980aea4bb91509cb0
Manoj Chowdhury <manojc009@gmail.com>
2019-11-06 23:11:22 +0100

    added life-cycle hooks

--- src/containers/App.js
+++ src/containers/App.js
@@ -4,6 +4,11 @@ import Cockpit from '../components/Cockpit/Cockpit';
 import Persons from '../components/Persons/Persons';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    console.log('In constructor');
+  }
+
   state = {
     persons: [
       { id:'asdhuih', name: 'Manoj', age: 26 },
@@ -11,6 +16,15 @@ class App extends Component {
       { id:'asasuih', name: 'MKC', age: 29 }
     ],
     showPersons: false
+  };
+
+  static getDerivedStateFromProps(props, state){
+    console.log('getDerivedStateFromProps',props)
+    return state;
+  }
+
+  componentDidMount(){
+    console.log('componentDidMount');
   }
 
   deletePersonHandler = (personIndex) => {
@@ -39,6 +53,7 @@ class App extends Component {
   }
 
   render() {
+    console.log('In App render');
     let persons = null;
     
     if( this.state.showPersons ){
@@ -51,6 +66,7 @@ class App extends Component {
     return (
       <div className={classes.App}>
         <Cockpit
+          title={this.props.appTitle}
           showPersons={this.state.showPersons}
           persons={this.state.persons}
           clicked={this.togglePersonsHandler}/>
